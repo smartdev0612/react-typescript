21820843961bd31698a73777ba72a215c72e63fc
Daniel Morales <danieldev0612@gmail.com>
2022-05-17 11:56:57 -0700

    initial commit

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react';
+
+import './NewTodo.css';
+
+type NewTodoProps = {
+    onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = ({ onAddTodo }) => {
+    const textInputRef = useRef<HTMLInputElement>(null);
+
+    const todoSubmitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = textInputRef.current!.value;
+        onAddTodo(enteredText);
+    };
+
+    return (
+        <form onSubmit={todoSubmitHandler}>
+            <div>
+                <label htmlFor="todo-text">Todo Text</label>
+                <input type="text" id="todo-text" className="form-control" ref={textInputRef} />
+            </div>
+            <button type="submit">ADD TODO</button>
+        </form>
+    );
+};
+
+export default NewTodo;
